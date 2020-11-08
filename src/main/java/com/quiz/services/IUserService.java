b33eae36940538de6cdb222ed36e65453b415fa1
van <vanpersieyolo@gmail.com>
2020-11-08 14:28:51 +0700

    done showAll,save,bug update

--- src/main/java/com/quiz/services/IUserService.java
+++ src/main/java/com/quiz/services/IUserService.java
@@ -1,4 +1,12 @@
 package com.quiz.services;
 
+import com.quiz.entities.App_User;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Optional;
+
 public interface IUserService {
+    App_User save(App_User user);
+    Iterable<App_User> findAll();
+    Optional <App_User> findById(Long id);
 }
